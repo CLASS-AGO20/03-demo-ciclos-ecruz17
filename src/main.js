cead711d9ff97b19eac9ed0be4694eade7cfd70b
ecruz17 <ecruz17@ucol.mx>
2020-12-05 23:02:04 -0600

    implemented function contarImparesFor

--- src/main.js
+++ src/main.js
@@ -11,9 +11,25 @@ export default class App {
         return res;
     }
 
+    contarImparesFor(inicio, fin) {
+
+        let impares = 0;
+
+        for(let i = inicio; i <= fin; i= i + 1) {
+            if(i%2 != 0) {
+            impares = impares + 1;
+            }
+        }
+
+        return impares;
+
+    }
+
 
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor())
+
+console.log(app.contarImparesFor(5,10))
