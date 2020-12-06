12be21d936710654799f0851e11ad12f019d08d0
ecruz17 <ecruz17@ucol.mx>
2020-12-05 22:29:22 -0600

    first function tested and programmed

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
-export default class App {}
+export default class App {
+
+    sumarParesFor() {
+        let res = 0;
+
+        for(let i = 1; i < 21;i = i + 1) {
+            if (i%2 == 0) {
+                res = res + i;
+            }
+        }       
+        return res;
+    }
+
+
+}
 
 let app = new App();
+
+console.log(app.sumarParesFor())
