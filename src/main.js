c32feaea09c527bfeb54beed3344818109c5c92c
ecruz17 <ecruz17@ucol.mx>
2020-12-06 18:20:14 -0600

    implemented imparesWhile function and tested

--- src/main.js
+++ src/main.js
@@ -44,13 +44,32 @@ export default class App {
 
     }
 
+    contarImparesWhile(inicio, fin) {
+
+        let i = inicio;
+        let res = 0;
+
+        while(i <= fin) {
+
+            if(i%2 != 0) {
+                res = res + 1;
+            }
+
+            i = i + 1;
+        }
+   
+        return res;
+
+    }
 
 }
 
 let app = new App();
 
-console.log(app.sumarParesFor())
+console.log(app.sumarParesFor());
 
-console.log(app.contarImparesFor(5,10))
+console.log(app.contarImparesFor(5,10));
 
-console.log(app.sumarParesWhile())
+console.log(app.sumarParesWhile());
+
+console.log(app.contarImparesWhile(5,10));
